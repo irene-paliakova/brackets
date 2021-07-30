bd2b5ff7b908dbc5c5df6e600799332f92038ef6
Irina Paliakova <76568191+irene-paliakova@users.noreply.github.com>
2021-07-30 12:39:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  let wasChanged = true;
+  while (wasChanged) {
+    wasChanged = false;
+    for (let i = 0; i < str.length - 1; i++) {
+      for (let ii = 0; ii < bracketsConfig.length; ii++){
+        if (str[i] == bracketsConfig[ii][0] && str[i+1] == bracketsConfig[ii][1]) {
+          str = str.replace(str[i] + str[i+1], '');
+          wasChanged = true;
+          break;
+        }
+        if (wasChanged)
+          break;
+      }
+    }
+  }
+  if (str.length == 0)
+    return true;
+  else 
+    return false;
 }
